2f729ef4f30c8fb59d107ffd5f99e8f4a954a7f6
Victor Perez <vperezma89@gmail.com>
2016-01-18 22:49:10 -0800

    working beginning routing.

--- app/views/app.jsx
+++ app/views/app.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { render } from 'react-dom'
 import '~/app/assets/styles/app'
+import {Router, Route, browserHistory} from 'react-router'
+import login from './login'
+import home from './index'
 
 class App extends React.Component {
   render() {
-    const { children } = this.props
+    const { children } = this.props;
 
     return (
       <div className='container'>
@@ -15,4 +18,12 @@ class App extends React.Component {
   }
 }
 
-render(<App />, document.getElementById('react'))
+render((
+    <Router history = {browserHistory}>
+      <Route path="/" component={App}>
+        <Route path="login" component={login}/>
+
+      </Route>
+    </Router>
+
+    ), document.getElementById('react'));
---
+++ app/views/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default class homeIndex extends React.Component {
+
+    render() {
+        return (
+            <div>Index</div>
+        )
+    }
+}
